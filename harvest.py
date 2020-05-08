e075cb24b75ad9aefd851f24b99650cc7d3c5026
Melissa Wiseman Rauch <melissa.wiseman@gmail.com>
2020-05-08 03:25:03 +0000

    Created all melon types list

--- harvest.py
+++ harvest.py
@@ -10,42 +10,49 @@ class MelonType(object):
                  name):
         """Initialize a melon."""
 
-        self.name = [name]
-        self.code = [code]
-        self.first_harvest = [first_harvest]
-        self.color = [color]
+        self.name = name
+        self.code = code
+        self.first_harvest = first_harvest
+        self.color = color
         self.pairings = []
-        self.is_seedless = [is_seedless]
-        self.is_bestseller = [is_bestseller]
+        self.is_seedless = is_seedless
+        self.is_bestseller = is_bestseller
         
 
     def add_pairing(self, pairing):
         """Add a food pairing to the instance's pairings list."""
-
-        name = self.name.upper()
-        print(f'{self.name} pairs well with {self.pairings}.')
+        self.pairings = self.pairings.append(pairings)
+        return self.pairings
 
     def update_code(self, new_code):
         """Replace the reporting code with the new_code."""
 
         # Fill in the rest
         self.code = self.new_code
-        print(f'{self.code} updated to {self.new_code}')
-
+        return self.code
 
 def make_melon_types():
     """Returns a list of current melon types."""
 
     all_melon_types = []
-
+# code, first_harvest, color, is_seedless, is_bestseller, 
+#                  name)
     # Fill in the rest
-
-    return all_melon_types
+    "Muskmelon" = MelonType("musk", 1998, "green", True, True, "Muskmelon")
+    "Casaba" = MelonType("cas", 2003, "orange", False, False, "Casaba")
+    "Crenshaw" = MelonType("cren", 1996, "green", False, False, "Crenshaw")
+    "Yellow Watermelon" = MelonType("yw", 2013, "yellow", False, False, "Yellow Watermelon")
+    for melon in MelonType:
+        all_melon_types.append(melon)
+    print(all_melon_types)
 
 def print_pairing_info(melon_types):
     """Prints information about each melon type's pairings."""
 
     # Fill in the rest
+    for melon in melon_types:
+        melon = self.name.upper()
+        print(f'{self.name} pairs well with {self.pairings}.')
 
 def make_melon_type_lookup(melon_types):
     """Takes a list of MelonTypes and returns a dictionary of melon type by code."""
